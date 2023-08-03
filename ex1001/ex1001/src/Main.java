2add4dc72293d2c15343384099cee1b897e1f387
VictorPera <victorhugo.pera@gmail.com>
2023-08-03 03:01:58 -0300

    Exercício 1001 concluído

---
+++ ex1001/ex1001/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.print("Digite o valor de A: ");
+        int A = sc.nextInt();
+        // System.out.print("Digite o valor de B: ");
+        int B = sc.nextInt();
+
+        int X;
+
+        X = A + B;
+
+        System.out.println("X = " + X);
+
+    }
+}
